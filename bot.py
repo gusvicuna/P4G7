2e80dbb5eb34d3f677f50627eae3f756190c7531
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2021-11-11 11:51:20 -0300

    update

--- bot.py
+++ bot.py
@@ -6,9 +6,41 @@ from bottle import (
 )
 
 BOT_URL = 'https://api.telegram.org/bot2105695159:AAGP21o80wOJeBFXHDGTX7n7aJxAAEm3jwg/'  # <--- add your telegram token here; it should be like https://api.telegram.org/bot12345678:SOMErAn2dom/
-questions_list = ["¿Cómo se definen las variables?", "¿Cómo es el formato del 'if'? ", "¿Cual es el prefijo para crear una funcion?"]
-options_list = [["nombre_variable: valor", "valor: nombre_variable", "nombre_variable= valor", "valor = nombre_variable"], ["if condicion:", "if(condicion){}", "if:", "if[condicion]"],["void","fun","def","function"]]
-answer_list = [2, 0, 2]
+questions_list = ["¿Cómo se definen las variables?", "¿Cómo es el formato del 'if'? ",
+                  "¿Cual es el prefijo para crear una funcion?", "Cual es el resultado de print(list(range(1,5)))",
+                  "Cual es el resultado de la operacion  'r' * 3 "]
+options_list = [
+    ["nombre_variable: valor", "valor: nombre_variable", "nombre_variable= valor", "valor = nombre_variable"],
+    ["if condicion:", "if(condicion){}", "if:", "if[condicion]"], ["void", "fun", "def", "function"],
+    ["[1, 2, 3, 4]", "{1, 2, 3, 4,}", "{1, 2, 3, 4, 5}", "[1, 2, 3, 4, 5]"],
+    ["Error", "'rrr'", "Invalid Operation", "'r'"]]
+answer_list = [2, 0, 2, 0, 1]
+
+
+class PreguntaAlternativa:
+    question = ""
+    correct_answer = 0
+    answers = []
+    open_period = 20
+
+    def __init__(self, question, correct_answer, answers,open_period=20):
+        self.question = question
+        self.correct_answer = correct_answer
+        self.answers = answers
+        self.open_period = open_period
+
+
+preguntas_lvl1 = [PreguntaAlternativa("¿Cómo se definen las variables?", 2,
+                                      ["nombre_variable: valor", "valor: nombre_variable",
+                                       "nombre_variable= valor", "valor = nombre_variable"]),
+                  PreguntaAlternativa("¿Cual es el prefijo para crear una funcion?", 0,
+                                      ["if condicion:", "if(condicion){}", "if:", "if[condicion]"]),
+                  PreguntaAlternativa("¿Cual es el prefijo para crear una funcion?", 2,
+                                      ["void", "fun", "def", "function"]),
+                  PreguntaAlternativa("Cual es el resultado de print(list(range(1,5)))", 0,
+                                      ["[1, 2, 3, 4]", "{1, 2, 3, 4,}", "{1, 2, 3, 4, 5}", "[1, 2, 3, 4, 5]"]),
+                  PreguntaAlternativa("Cual es el resultado de la operacion  'r' * 3 ", 1,
+                                      ["Error", "'rrr'", "Invalid Operation", "'r'"])]
 
 
 def get_chat_id(data):
@@ -51,7 +83,8 @@ def process_data(data):
                 "question": question,
                 "options": options,
                 "type": "quiz",
-                "correct_option_id": answer
+                "correct_option_id": answer,
+                "open_period": 30
             }
 
             send_message(json_data)  # <--- function for sending answer
