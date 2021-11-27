9878db29784d1c1048c2aedaa0eebb7805637118
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2021-11-27 16:24:27 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ answer_list = [2, 0, 2, 0, 1]
 
 class MChoiceQuestion:
     question = ""
-    correct_answer = 0
+    correct_answer = ""
     options = []
     open_period = 20
 
@@ -56,7 +56,7 @@ def send_message(prepared_data):
     """
     Prepared data should be json which includes at least `chat_id` and `text`
     """
-    message_url = BOT_URL + 'sendPoll'
+    message_url = BOT_URL + 'sendMessage'
     requests.post(message_url, json=prepared_data)  # don't forget to make import requests lib
 
 
@@ -64,22 +64,27 @@ def process_data(data):
     if "message" in data and "text" in data["message"]:
         print(data)
         db_values = check_chat_id(data['message']['chat']['id'])
+
         if data["message"]["text"] == "/save" and data['message']['chat']['id'] == -755520407:
             save_db()
+
         if data["message"]["text"] == "/pregunta":
             random_number = random.randint(0, len(mChoice_questions_lvl1[0]) - 1)
             question = mChoice_questions_lvl1[0][random_number]
 
             json_data = {
                 "chat_id": data['message']['chat']['id'],
-                "question": question.question,
-                "options": question.options,
-                "type": "quiz",
-                "correct_option_id": question.correct_answer,
-                "open_period": 30
+                "text": "{0}\nA){1}\nB){2}\nC){3}\nD){4}".format(question.question, question.options[0],
+                                                                 question.options[1], question.options[2],
+                                                                 question.options[3]),
+                "reply_markup": {"keyboard": [[{"text": "A"}], [{"text": "B"}], [{"text": "C"}], [{"text": "D"}]],
+                                 "one_time_keyboard": True}
             }
 
             send_message(json_data)  # <--- function for sending answer
+
+        if "reply_to_message" in data["message"]:
+            return
     elif "poll" in data:
         print(data)
         return
