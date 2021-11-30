399e2e6094b2b5d55aa3fb62558bc3d3d7173d75
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2021-11-30 03:19:23 -0300

    add code questions

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 x = 0
+a = 0
+b = 0
+c = 0
+d = 0
+y = 0
+z = 0
+
+correct_answer = "8765432"
+pre_exec = "global a; a = '123456789'"
+
+test_answer = "x = a[::-1][1:-1]"
 
 def process_code_answer(answer):
     exec("global x; " + answer)
     return x
 
-test_answer = "x = 2"
-print(process_code_answer(test_answer))
+
+exec(pre_exec)
+print(str(process_code_answer(test_answer)), correct_answer)
+print(str(process_code_answer(test_answer)) == correct_answer)
